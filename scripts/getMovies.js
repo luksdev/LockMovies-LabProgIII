77e75c17fdb8d8ef843a26a6f14d713c75998b23
Lucas Eduardo <69233035+luksdev@users.noreply.github.com>
2022-02-27 00:55:11 -0300

    add feature search movie

--- scripts/getMovies.js
+++ scripts/getMovies.js
@@ -1,8 +1,34 @@
-import 'dotenv/config'
-import fetch from "node-fetch";
+import useState from './useState.js'
 
-const key = process.env.API_MOVIES_KEY
+const KEY = "da4c8a4fb00b774b6b81692089eed935"
+const setMovieName = document.getElementById('searchCheck')
+const inputSearch = document.getElementById('SearchInput')
+const buttonSearch = document.getElementById('ButtonSearch')
+const searchReturn = document.getElementById('SearchResult')
 
-fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${key}`)
+// Pegar título por ID
+const getTitle = () => {
+    fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${KEY}&language=pt-BR`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data.title)
+        buttonSearch.addEventListener("click", function(){setMovieName.innerText = data.title})
+    })
+}
+
+// Realizar a pesquisa
+buttonSearch.addEventListener('click', () => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${inputSearch.value}`)
+    .then(res => res.json())
+    .then(data => {     
+        const arrayResults = data['results']        
+        const mapMovies = arrayResults.map(function(movies, index){
+            return `<li>${movies.title}</li>`
+        })
+        searchReturn.innerHTML = mapMovies
+    })
+})
+
+
+getTitle()
+
