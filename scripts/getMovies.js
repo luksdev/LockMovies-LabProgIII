203ee596f15df156284d00f9c2e1f40e5f861566
luksdev <lucasgamer4i50@gmail.com>
2022-02-28 15:17:34 -0300

    update styles and scripts

--- scripts/getMovies.js
+++ scripts/getMovies.js
@@ -1,34 +1,37 @@
 import useState from './useState.js'
 
+// Variables
 const KEY = "da4c8a4fb00b774b6b81692089eed935"
 const setMovieName = document.getElementById('searchCheck')
 const inputSearch = document.getElementById('SearchInput')
 const buttonSearch = document.getElementById('ButtonSearch')
 const searchReturn = document.getElementById('SearchResult')
 
-// Pegar título por ID
+// gey title by ID
 const getTitle = () => {
     fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${KEY}&language=pt-BR`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.title)
-        buttonSearch.addEventListener("click", function(){setMovieName.innerText = data.title})
+        // buttonSearch.addEventListener("click", function(){setMovieName.innerText = data.title})
     })
 }
 
 // Realizar a pesquisa
-buttonSearch.addEventListener('click', () => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${inputSearch.value}`)
-    .then(res => res.json())
-    .then(data => {     
-        const arrayResults = data['results']        
-        const mapMovies = arrayResults.map(function(movies, index){
-            return `<li>${movies.title}</li>`
+const search = () => {
+    buttonSearch.addEventListener('click', () => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${inputSearch.value}&language=pt-BR`)
+        .then(res => res.json())
+        .then(data => {     
+            const arrayResults = data['results']        
+            const mapMovies = arrayResults.map(function(movies, index){
+                console.log(movies.title)
+                // return `<li>${movies.title}</li>`
+            })
+            // searchReturn.innerHTML = mapMovies
         })
-        searchReturn.innerHTML = mapMovies
     })
-})
+}
 
-
+search()
 getTitle()
 
